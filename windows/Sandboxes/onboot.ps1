92ac1f9ec6d812d46fb1644e11f6739487cebca9
Carlos Peña <Cdpm42@gmail.com>
2024-10-06 12:34:17 -0600

    Update onboot.ps1

--- windows/Sandboxes/onboot.ps1
+++ windows/Sandboxes/onboot.ps1
@@ -11,8 +11,22 @@ Add-AppxPackage $env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage $env:temp\Microsoft.UI.Xaml.2.8.x64.appx
 Add-AppxPackage $env:temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-$SoftwareToInstall = "Microsoft.VisualStudioCode", "qBittorrent.qBittorrent", "VideoLAN.VLC"
+#$SoftwareToInstall = "Microsoft.VisualStudioCode", "qBittorrent.qBittorrent", "VideoLAN.VLC"
+$SoftwareToInstall = "qBittorrent.qBittorrent"
 
 foreach ($Software in $SoftwareToInstall) {
     WinGet.exe install $software --silent --force --accept-source-agreements --disable-interactivity --source winget
 }
+
+$extensionID = "odfafepnkmbhccpbejgmiehpchacaeak"
+$updateURL = "https://edge.microsoft.com/extensionwebstorebase/v1/crx"
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist"
+
+# Create the registry path if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force
+}
+
+# Add the uBlock Origin extension to the force install list
+$regValue = "$extensionID;$updateURL"
+New-ItemProperty -Path $regPath -Name "1" -Value $regValue -PropertyType String -Force
